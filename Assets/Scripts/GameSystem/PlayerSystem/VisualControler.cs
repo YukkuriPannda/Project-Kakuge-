327f963abfdf8f63f124b725e2080a485b70ca99
YukkuriPannda <funa.hayate@gmail.com>
2022-04-29 11:56:17 +0900

    visualconの反転実装

--- Assets/Scripts/GameSystem/PlayerSystem/VisualControler.cs
+++ Assets/Scripts/GameSystem/PlayerSystem/VisualControler.cs
@@ -4,35 +4,53 @@ using UnityEngine;
 
 public class VisualControler : MonoBehaviour
 {
-    /*
-    0 = stay
-    1 = RUN
-    2 = 
-    */
     public GameObject Player;
-    int stayMotionNumber;
     private Animator playerAnimator;
     private PlayerController playerController;
+    enum AnimMotions :int {
+        Stay,
+        Walk,
+        Run,
+        Jump,
+        SwordAttack,
+        MagicAttack
+    }
+    enum Orientation{
+        Right,Left
+    }
     private void Start() {
         playerAnimator = Player.GetComponent<Animator>();
         playerController = Player.GetComponent<PlayerController>();
-
-        playerAnimator.SetInteger("AnimNumber",0);
+        playerAnimator.SetInteger("AnimNumber",(int)AnimMotions.Stay);
+        playerAnimator.SetInteger("Orientation",(int)Orientation.Right);
     }
     void Update()
     {
-            playerAnimator.SetInteger("AnimNumber",0);
-        if(playerController.rb2D.velocity.x != 0){
-            playerAnimator.SetInteger("AnimNumber",1);
+        playerAnimator.SetInteger("AnimNumber",(int)AnimMotions.Stay);
+        switch(playerController.rb2D.velocity.x){
+            case float f when (f>2f):
+            playerAnimator.SetInteger("AnimNumber",(int)AnimMotions.Run);
+            playerAnimator.SetInteger("Orientation",(int)Orientation.Right);
+            break;
+            case float f when(f<-2f):
+            playerAnimator.SetInteger("AnimNumber",(int)AnimMotions.Run);
+            playerAnimator.SetInteger("Orientation",(int)Orientation.Left);
+            break;
+            case float f when(f>0):
+            playerAnimator.SetInteger("AnimNumber",(int)AnimMotions.Walk);
+            playerAnimator.SetInteger("Orientation",(int)Orientation.Right);
+            break;
+            case float f when(f<0):
+            playerAnimator.SetInteger("AnimNumber",(int)AnimMotions.Walk);
+            playerAnimator.SetInteger("Orientation",(int)Orientation.Left);
+            break;
+            case float f when(f==0):
+            playerAnimator.SetInteger("AnimNumber",(int)AnimMotions.Stay);
+            break;
         }
     }
     void StayMotion() //ランダムにモーション変化を分岐させる
     {
-        stayMotionNumber = Random.Range(1,10);
-        switch(stayMotionNumber){
-            case  int n when n <= 5:
-            playerAnimator.SetInteger("StayAnimNumber",0);
-            break;
-        }
+       
     }
 }

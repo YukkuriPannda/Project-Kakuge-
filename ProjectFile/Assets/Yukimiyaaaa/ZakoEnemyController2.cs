f56cc7ea9835dfb25d3f217b52ef60adba7bcdbe
Yukimiya36 <mayumi.frisk.20060408@gmail.com>
2023-02-26 17:17:49 +0900

    idou

--- ProjectFile/Assets/Yukimiyaaaa/ZakoEnemyController2.cs
+++ ProjectFile/Assets/Yukimiyaaaa/ZakoEnemyController2.cs
@@ -7,9 +7,12 @@ public class ZakoEnemyController2 : MonoBehaviour
 {
     private float detecitrRadius=3.0f;
     private float attackRadius = 1.0f;
-    private Vector2 pos;
+    private Vector2 StartPos;
     private int num=1;
     private int count;
+    private Vector2 PlayerPosition;
+    private Vector2 EnemyPosition;
+    private GameObject playerObject;
     public Rigidbody2D rb2D;
     public GameObject target;//(target=Player)
     public float movementSpeed=1.0f;
@@ -18,28 +21,40 @@ public class ZakoEnemyController2 : MonoBehaviour
     void Start()
     {
         this.rb2D=GetComponent<Rigidbody2D>();
-        // if(PlayerがdetectirRadius以内&&attackRadius以内なら)
-        if(gameObject.CompareTag("Player")==(detecitrRadius<=3.0f)&&(attackRadius<=1.0f))
-        {
-            GameObject.FindGameObjectsWithTag("Player");
-            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, 3 * Time.deltaTime);
-        }
+        playerObject = GameObject.FindWithTag("Player");
+        StartPos=transform.position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(Mathf.Abs(playerObject.transform.position.x - transform.position.x)<=detecitrRadius)
+        {
+            //追尾
+        PlayerPosition = playerObject.transform.position;
+        EnemyPosition = transform.position;
+        
+        if (PlayerPosition.x > EnemyPosition.x)
+        {
+            rb2D.AddForce(new Vector2(rb2D.mass * (1 * movementSpeed - rb2D.velocity.x) , 0));
+        }
+        else if (PlayerPosition.x < EnemyPosition.x)
+        {
+            rb2D.AddForce(new Vector2(rb2D.mass * (-1 * movementSpeed - rb2D.velocity.x) , 0));
+        }
+        }else{
         //常に往復運動
-        pos=transform.position;
         rb2D.AddForce(new Vector2(rb2D.mass * (num * movementSpeed - rb2D.velocity.x) , 0));
 
-        if(pos.x>2)
+        if(transform.position.x - StartPos.x>2)
         {
             num=-1;
         }
-        else if(pos.x<-2)
+        else if(transform.position.x - StartPos.x<-2)
         {
             num=1;
         }
+        }
+        
     }
 } 

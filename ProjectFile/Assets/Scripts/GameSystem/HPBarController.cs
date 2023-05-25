3f33597729bd96a7398c533a8c706bee56d1e5e9
YukkuriPannda <100471518+YukkuriPannda@users.noreply.github.com>
2023-05-25 22:49:44 +0900

    Add Minimum DMG Text

--- ProjectFile/Assets/Scripts/GameSystem/HPBarController.cs
+++ ProjectFile/Assets/Scripts/GameSystem/HPBarController.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HPBarController : MonoBehaviour
 {
     public EntityBase entityBase;
     public RectTransform mainBarRectTrf;
     public RectTransform subBarRectTrf;
+    public GameObject DMGTexPrefab;
     private RectTransform my_rect_trf;
     private Camera main_camera;
+    private float oldHP = 0;
+
     void Start()
     {
         my_rect_trf = gameObject.GetComponent<RectTransform>();
@@ -16,12 +20,15 @@ public class HPBarController : MonoBehaviour
     }
     void Update()
     {
-        /*
-        if(entityBase.Health >= 0)mainBar.transform.localScale = new Vector3(entityBase.Health/entityBase.MaxHealth*10,0.75f,1);
-        if(subBar.transform.localScale.x > 0&&mainBar.transform.localScale.x < subBar.transform.localScale.x)subBar.transform.localScale -= new Vector3(1.5f*Time.deltaTime,0,0);
-        */
         if(entityBase.Health >= 0)mainBarRectTrf.sizeDelta = new Vector2(entityBase.Health/entityBase.MaxHealth*20,2);
         if(subBarRectTrf.sizeDelta.x > 0&&mainBarRectTrf.sizeDelta.x < subBarRectTrf.sizeDelta.x)subBarRectTrf.sizeDelta -= new Vector2(5f*Time.deltaTime,0);
         my_rect_trf.position = RectTransformUtility.WorldToScreenPoint(main_camera,entityBase.transform.position);
+        if(oldHP > entityBase.Health){
+            GameObject DMGTexObject = Instantiate(DMGTexPrefab,transform.position + new Vector3(20,0,0),Quaternion.identity,transform);
+            TextMeshProUGUI DMGTex = DMGTexObject.GetComponent<TextMeshProUGUI>();
+            DMGTex.text = (oldHP-entityBase.Health).ToString();
+            Destroy(DMGTexObject,1);
+        }
+        oldHP = entityBase.Health;
     }
 }

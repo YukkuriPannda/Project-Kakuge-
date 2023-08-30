ba2c81ad44a2542cdd80b8945c6f11998d1fba4c
YukkuriPannda <100471518+YukkuriPannda@users.noreply.github.com>
2023-08-30 15:32:14 +0900

    NPC system

--- ProjectFile/Assets/Scripts/System/IntaractiveObject/InteractiveBase.cs
+++ ProjectFile/Assets/Scripts/System/IntaractiveObject/InteractiveBase.cs
@@ -6,15 +6,18 @@ public class InteractiveBase : MonoBehaviour
 {
     [ReadOnly]private bool touchng;
     public KeyCode key;
+    public List<PlayerController> touchingPlc = new List<PlayerController>();
     void OnTriggerEnter2D(Collider2D collision)
     {
+        touchingPlc.Add(collision.gameObject.GetComponent<PlayerController>());
         if (collision.gameObject.CompareTag("Player")){
-            OnEnterPlayer(collision.gameObject.GetComponent<PlayerController>());
+            OnEnterPlayer(touchingPlc[touchingPlc.Count - 1]);
             touchng = true;
         }
     }
     void OnTriggerExit2D(Collider2D collision)
     {
+        touchingPlc.Remove(collision.gameObject.GetComponent<PlayerController>());
         if (collision.gameObject.CompareTag("Player")){
             OnExitPlayer(collision.gameObject.GetComponent<PlayerController>());
             touchng = false;
--- ProjectFile/Assets/Scripts/System/IntaractiveObject/NPCController.cs
+++ ProjectFile/Assets/Scripts/System/IntaractiveObject/NPCController.cs
@@ -5,13 +5,43 @@ using TMPro;
 
 public class NPCController : InteractiveBase {
     public float textInterval;
-    public string text;
+    public bool openningMessageBox;
+    public string[] TalkTexts;
     public GameObject messageBox;
     public GameObject keyBox;
     public GameObject actionName;
     public TextMeshProUGUI bodyTex;
+    public Animator animator;
     public override IEnumerator Action_IE(){
-        messageBox.GetComponent<Animator>().Play("Enable",1,0);
+        if(!openningMessageBox){
+            bodyTex.gameObject.SetActive(true);
+            animator.Play("Enable",1,0);
+            openningMessageBox = true;
+            touchingPlc[touchingPlc.Count - 1].lockOperation = true;
+            for(int i = 0;i < TalkTexts.Length;i ++){
+                yield return StartCoroutine(Talk(TalkTexts[i]));
+            }
+            touchingPlc[touchingPlc.Count - 1].lockOperation = false;
+            openningMessageBox = false;
+            animator.gameObject.SetActive(false);
+        }
+        yield break;
+    }
+    public override void OnEnterPlayer(PlayerController plc){
+        keyBox.SetActive(true);
+        actionName.SetActive(true);
+        animator.Play("Activation",0,0);
+    }
+    public override void OnExitPlayer(PlayerController plc){
+        StartCoroutine(Disable());
+    }
+    IEnumerator Disable(){
+        animator.Play("Disable",0,0);
+        yield return new WaitForSeconds(0.1f);
+        keyBox.SetActive(false);
+        actionName.SetActive(false);
+    }
+    public IEnumerator Talk(string text){
         bodyTex.text = text;
         messageBox.GetComponent<RectTransform>().sizeDelta = new Vector2(bodyTex.preferredWidth+10,bodyTex.preferredHeight+10);
         bodyTex.text = "";
@@ -19,20 +49,7 @@ public class NPCController : InteractiveBase {
             bodyTex.text += text[i];
             yield return new WaitForSeconds(textInterval);
         }
+        while(! Input.anyKeyDown)yield return null;
         yield break;
-    }
-    public override void OnEnterPlayer(PlayerController plc){
-        keyBox.SetActive(true);
-        actionName.SetActive(true);
-        messageBox.GetComponent<Animator>().Play("Activation",0,0);
-    }
-    public override void OnExitPlayer(PlayerController plc){
-        StartCoroutine(Disable());
-    }
-    IEnumerator Disable(){
-        messageBox.GetComponent<Animator>().Play("Disable",0,0);
-        yield return new WaitForSeconds(0.1f);
-        keyBox.SetActive(false);
-        actionName.SetActive(false);
     }
 }

ff4ae925d778133783b8c592dcac1b6966274375
Yukimiya <121948741+Yukimiya36@users.noreply.github.com>
2023-10-30 00:42:22 +0900

    EarthWall2

--- ProjectFile/Assets/Scripts/Magics/SpecialMagic/EarthWall.cs
+++ ProjectFile/Assets/Scripts/Magics/SpecialMagic/EarthWall.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
-
-public class EarthWall : PlayerMagicBase
+public class EarthWall: PlayerMagicBase
 {
     // スキル種別
     public override PlayerMagicFactory.MagicKind SkillKind
@@ -23,11 +23,30 @@ public class EarthWall : PlayerMagicBase
     }
     public override IEnumerator ActivationTerraMagic(PlayerController plc)
     {
-
-        GameObject.Destroy(GameObject.Instantiate((GameObject)Resources.Load("Magics/EarthWallPrefab"),plc.transform.position + new Vector3(0,0.3f,0),plc.transform.rotation), 10f);
+    int layermask = 1 << LayerMask.NameToLayer("Ground");
+    Vector3 playerPosition = plc.transform.position;
+    RaycastHit2D hit;
+    RaycastHit2D hitObject = Physics2D.CircleCast(plc.drawShapePos + (Vector2)plc.transform.position,0.3f,Vector2.down,Mathf.Infinity,layermask);
+    if (hitObject.collider)
+    {
+        Vector3 targetPosition = hitObject.point;
         
-        yield break;
+        if (Mathf.Abs(plc.drawShapePos.x) <= 3.0f)
+        { 
+            Debug.Log ("Earth");
+            Debug.Log(hitObject.point);
+            Vector3 wallSpawnPosition = hitObject.point;
+            GameObject.Destroy(GameObject.Instantiate((GameObject)Resources.Load("Magics/EarthWallPrefab"),wallSpawnPosition + new Vector3(0,0.7f), plc.transform.rotation), 10f);
+        }
+        else
+        {
+            Debug.Log("out");
+            Vector3 directionoTarget = (targetPosition - playerPosition).normalized;
+            Vector3 wallSpawnPosition = directionoTarget * 3.0f;
+            GameObject.Destroy(GameObject.Instantiate((GameObject)Resources.Load("Magics/EarthWallPrefab"),wallSpawnPosition + new Vector3(0,0.7f), plc.transform.rotation), 10f);
+        }
     }
+    yield break;
+}
 
-    
 }
